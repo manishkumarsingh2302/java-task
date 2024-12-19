bc57d0c1f5ba1662fb281aa4cef7e74d2db3d371
manishkumarsingh2302 <manishkumarbkj2005@gmail.com>
2024-12-19 13:08:30 +0530

    succes

---
+++ Ad1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Ad1{
+     public static void main(String args []){
+         int A[][]=new int[2][5];
+   int r,c;
+Scanner k=new Scanner(System.in);
+    for(r=0;r<A[r].length;c++){
+System.out.println("enter elament Index of "+r+c+":");
+A[r][c]=k.nextInt();
+}
+
+}
+System.oy.println(" output of 2d arry :");
+  for(r=0;r<A[r].length;c++){
+System.out.println("" +A[r][c]);
+
+}
+System.out.println(" ");
+}
+}
+}
