7fcc58e3f24521df8d9302f1a7a0a6194bd9ca2a
manishkumarsingh2302 <manishkumarbkj2005@gmail.com>
2024-12-19 13:10:11 +0530

    succes

---
+++ Add2d.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+class Add2d{
+            public static void main(String args[]){
+            int A[][]=new int [3][3];
+            
+           int r,c;
+      Scanner sk=new Scanner(System.in);
+     for(r=0;r<3;r++){
+                    for(c=0;c<3;c++){
+         System.out.println{"enter elament"+r+c":"};
+         A[r][c]=sk.nextInt();
+     
+    }
+}
+  int sum;
+ for(r=0;r<3;r++){
+     sum=0;
+             for(c=0;c<3;c++){
+            System.out.print("\t" +sum);
+}
+int nums[]=new int[3];
+ for(r=0;r<3;r++){
+sum=0;
+     for(c=0;c<3;c++)
+sum=sum+A[]c[r];
+}
+  nums[r]=sum;
+System.out.println("");
+}
+for(int i=0;i<nums.length;i++){
+System.out.println("\t"}+nums[i]);
+}
+}
+}
+
---
+++ StudentInfo.java
@@ -0,0 +1,31 @@
+class StudentInfo{
+public static void main(String args[]){
+String name;
+String enroll;
+char s;
+int m;
+int p;
+int c;
+int h;
+int e;
+int total;
+float per;
+name="Manish";
+enroll="01152221212";
+s='A';
+m=88;
+p=67;
+c=78;
+h=66;
+e=56;
+  total=s+m+p+c+e;
+  per=total/5.0f;
+   System.out.println("Name: "+name);
+   System.out.println("Enrollment: "+enroll);
+   System.out.println("Section: "+s);
+   System.out.println("Math: "+m);
+   System.out.println("chemestry: "+c);
+   System.out.println("Hindi: "+h);
+   System.out.println("English: "+e);
+}
+}
