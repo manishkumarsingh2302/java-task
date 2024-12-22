10604ce34a6066919efa30d0fb490b84f8f35859
manishkumarsingh2302 <manishkumarbkj2005@gmail.com>
2024-12-22 23:39:00 +0530

    22/12/2024

---
+++ A21.java
@@ -0,0 +1,17 @@
+// compair two String
+    class A21{
+          public static void main (String [] args){
+          String name="manish";
+          String newname="Ekta";
+  System.out.println(name);
+  System.out.println(newname);
+  
+
+ if(name.equals(newname)){
+  System.out.println("both are eqal");
+    }
+else{
+    System.out.println("both are  not eqal");
+}
+}
+}
