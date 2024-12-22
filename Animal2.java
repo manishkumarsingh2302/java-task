ee3bc87d5dfe883d55c144972bfe0eeb32d9d38a
manishkumarsingh2302 <manishkumarbkj2005@gmail.com>
2024-12-23 03:13:23 +0530

    succes

---
+++ Animal2.java
@@ -0,0 +1,18 @@
+//oops concept ,learn how to make object and class and call?
+  class Animal{
+           public void eat()
+{
+   System.out.println("i am eating");
+}
+  public static void main(String args[]){
+    System.out.println("manish");
+    Animal dog=new Animal();
+    dog.eat();
+    dog.run();
+
+}
+public void run();
+{
+   System.out.println("aman is running")
+}
+}
