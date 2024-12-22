bff1489f5a0dced8aacb66ca08c9f4f086797c96
manishkumarsingh2302 <manishkumarbkj2005@gmail.com>
2024-12-22 23:57:36 +0530

    succes

---
+++ a23.java
@@ -0,0 +1,17 @@
+// compair two String .......eqalsIgnoreCase ? 
+    class a23{
+          public static void main (String [] args){
+          String name="manish";
+          String newname="MaNish";
+  System.out.println(name);
+  System.out.println(newname);
+  
+
+ if(name.equalsIgnoreCase(newname)){
+  System.out.println("both are eqal");
+    }
+else{
+    System.out.println("both are  not eqal");
+}
+}
+}
