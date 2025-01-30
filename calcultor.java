316458d9c842d73eba9c651d210be7384ea6bfb1
manishkumarsingh2302 <manishkumarbkj2005@gmail.com>
2025-01-30 23:05:44 +0530

    succes

---
+++ calcultor.java
@@ -0,0 +1,22 @@
+Import java.utill.Scannar;
+claas calcultor;
+{
+         public static void main(String[]args){
+      
+         Scannar s =newScannar(System.in);
+         
+         System.out.println("Enter a 1no: " );
+         int1no=s.next.int();
+         System.out.println("Enter a 2no: " );
+         int2no=s.next.int();
+         System.out.println(" select sym (+,-,*,%) ");
+         String sym=s.next();
+        int,res ;
+
+      Switch(sym)
+
+{     case: "+" res =1no+2no;
+       System.out.println(" Addition is : " +res)
+ break;  }
+}
+}
