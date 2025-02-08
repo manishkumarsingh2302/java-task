0cde9ef6ad4d5b457e7de711ce3e61e445339531
manishkumarsingh2302 <manishkumarbkj2005@gmail.com>
2025-02-08 19:38:59 +0530

    succes

---
+++ first.java
@@ -0,0 +1,15 @@
+class first{
+   public void myfunction (String sname,int sage){
+ System.out.println("the name is" +sname+ "age is " +sage);
+}
+public static void main(String args[])
+{
+ first f1=new first ();
+ f1.myfunction("lina",10);
+ first f2=new first ();
+ f1.myfunction("tina",20);
+ first f3=new first ();
+ f1.myfunction("kina",30);
+ }
+ }
+ 
