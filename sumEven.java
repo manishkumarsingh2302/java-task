0d13135407171663505c8a495758fafefd484ca3
manishkumarsingh2302 <manishkumarbkj2005@gmail.com>
2025-02-23 12:09:22 +0530

    succes

---
+++ sumEven.java
@@ -0,0 +1,12 @@
+class sumEven{
+    public static void main (String args[]){
+    int i;
+    int sum= 0;
+  for(i=0;i<12 ;i++)
+{
+   if (i%2==0)
+  sum=sum+i;
+}
+   System.out.println( "sum is total even no = "+sum);
+}
+}
