2f34a3eca0c637bb5e00a3d24217782d489c9bfa
manishkumarsingh2302 <manishkumarbkj2005@gmail.com>
2025-02-23 11:46:20 +0530

    succes

---
+++ sumTable.java
@@ -0,0 +1,15 @@
+ class sumTable
+             {
+            public static void main (String args[])
+{     
+          int i =1;
+         int sum=0;
+        while(i<11)
+{
+               sum=sum+i;
+                 i++;  
+}
+     System.out.println("total sum is=" + sum);
+}
+} 
+         
