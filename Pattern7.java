6023d0d20f14a8bac2e59d9f3f2be21357d4508a
manishkumarsingh2302 <manishkumarbkj2005@gmail.com>
2025-02-26 11:26:18 +0530

    ok

---
+++ Pattern7.java
@@ -0,0 +1,10 @@
+ class Pattern7 {
+    public static void main(String args[]) {
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*"); // Ek line mein stars print karega
+            }
+            System.out.println(); // New line ke liye
+        }
+    }
+}
