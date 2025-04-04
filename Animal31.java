13da31187c3dcaa093b86f995a2fe6e3134959f7
manishkumarsingh2302 <manishkumarbkj2005@gmail.com>
2025-04-04 15:12:20 +0530

    succes

---
+++ Animal31.java
@@ -0,0 +1,12 @@
+class Animal31{
+  public void eat(){
+     System.out.println("i am eating");
+}
+public static void main (String args[]){
+	System.out.println("halllo moto");
+ Animal31 dog=new Animal31();
+ dog.eat();
+}
+
+
+}
