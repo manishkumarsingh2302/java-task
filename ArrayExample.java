2a88796b6fa95d08eb73165d25a6e5fc5e10a223
manishkumarsingh2302 <manishkumarbkj2005@gmail.com>
2025-06-23 21:27:30 +0530

    done

---
+++ ArrayExample.java
@@ -0,0 +1,12 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        // Create an array of 5 integers
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Display the array elements
+        System.out.println("Array elements are:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
