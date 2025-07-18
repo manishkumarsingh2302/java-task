8848c21bd5fa0e5ca8e69f42327bed5e4eecc751
manishkumarsingh2302 <manishkumarbkj2005@gmail.com>
2025-07-18 13:05:09 +0530

    done

---
+++ Swap2number.java
@@ -0,0 +1,22 @@
+ //A normal question swap two number num1=10,num2=20; 
+ //YOUR OUTPUT IS NUM2=10,NUM1=20;
+
+
+  class Swap2number {
+        public static void main (String args[]){
+
+      int num1=10;
+      int num2=20;
+
+      int temp=num1;
+      num1=num2;
+      num2=temp;
+
+
+   System.out.println( "swaping result=" +num1);
+System.out.println( "swaping result=" +num2);
+
+}
+
+
+}
