689a827109420aa1edf0e6328eae8423d5d2fcb5
manishkumarsingh2302 <manishkumarbkj2005@gmail.com>
2025-09-18 10:25:47 +0530

    done

---
+++ Rive.java
@@ -0,0 +1,20 @@
+class Rive{
+    public static void main(String args[]){
+        int []a={1,3,2,5,6,7,9};
+        int i=0;
+        int j=a.length-1;
+        int temp;
+        while (i<j) {
+         temp=a[i];
+         a[i]=a[j];
+         a[j]=temp;           
+        
+        i++;
+        j--;
+        }
+        for(int k=0;k<a.length;k++){
+            System.out.print(a[k]+" ");
+        }
+    }
+
+}
